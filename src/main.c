8cd915e2d3d075196f64f6aeb85fa7befa811629
William Redenbaugh <wredenba@uci.edu>
2020-08-17 01:38:26 -0700

    made code more readable

--- src/main.c
+++ src/main.c
@@ -9,107 +9,39 @@
 // Our IOHandler libraries
 #include "io_handler.h"
 
-/*
-*   @brief attemps to get the file path for an image, and sends the struct over to parent function
-*   @returns image_info_t struct containing all the new image information
-*/
-image_info_t prompt_image_filepath(void){
-    image_info_t image_unpack; 
-    // Clear our the struct. 
-    memset(&image_unpack, 0, sizeof(image_unpack));
-    while(image_unpack.unpack_status != IMAGE_UNPACK_SUCCESS){
-        // Prompt our user for our file information
-        file_information_t file_info = prompt_file_information();
-        image_unpack = unpack_image(file_info.str_ptr, file_info.str_size); 
-        if(image_unpack.unpack_status != IMAGE_UNPACK_SUCCESS){
-            printf("Message couldn't be found, please try again. \n");    
-        }
-        free_file_information(&file_info); 
-    }
-    return image_unpack; 
-}
+uint8_t run(image_info_t *previous_image, image_info_t *current_image, image_info_t *original_image); 
+image_info_t prompt_image_filepath(void); 
+void prompt_file_write(image_info_t image_unpack); 
+void process_greyscale(image_info_t *previous_image, image_info_t *current_image, image_info_t *original_image); 
+void process_sharpen(image_info_t *previous_image, image_info_t *current_image, image_info_t *original_image); 
+void process_hue(image_info_t *previous_image, image_info_t *current_image, image_info_t *original_image); 
+void revert_checking(image_info_t *previous_image, image_info_t *current_image, image_info_t *original_image); 
 
-/*
-*   @brief Prompts the user for the file name that we want to save our new file in, and then writes the file
-*   @params image_info_t image_unpack The image that we want to save as a file.  
-*/
-void prompt_file_write(image_info_t image_unpack){
+int main(void){
+    // Since we need to have a file to unpack in the first place: 
+    image_info_t original_image = prompt_image_filepath();
+    // Our current image. 
+    image_info_t current_image = original_image; 
     
-    if(image_unpack.unpack_status != IMAGE_UNPACK_SUCCESS){
-        printf("File cannot be saved since image is unvalid\n");
-    }
+    // previous image is set to current image so that can't revert to NULL. 
+    // Limitation of program is that we can only go back one change, perhaps we can make an array 
+    // Linked list, but I don't think that is necesary. 
+    image_info_t previous_image = current_image; 
 
     while(1){
-        file_information_t file_info = prompt_file_information(); 
-        if(file_info.str_size > 0){
-            write_file(file_info.str_ptr, file_info.str_size, image_unpack); 
-            return; 
-        }
+        // If run is returned as 0, then we know to exit the program. 
+        if(!run(&previous_image, &current_image, &original_image))
+            return 0;  
     }
-}
-
-void process_greyscale(image_info_t *previous_image, image_info_t *current_image, image_info_t *original_image){
-    printf("Begin greyscale processing...");
-    image_info_t newimage = greyscale_image_data(*current_image);
-    // If the previous image isn't the original image. 
-    if(previous_image->image_dat.image_arr != original_image->image_dat.image_arr){
-        // If it isn't the original  image or the current image, we free up the heap memory block. 
-        if(previous_image->image_dat.image_arr != current_image->image_dat.image_arr)
-            free_image_data_mem(previous_image); 
-        
-        // After we have free the previous image data's block, save the previous 
-        // "current" image. 
-        previous_image = current_image; 
-    }
-    // The current image is then set to the new image. 
-    *current_image = newimage; 
-
-    printf("greyscale processing done\n"); 
-}
-
-void process_sharpen(image_info_t *previous_image, image_info_t *current_image, image_info_t *original_image){
-    printf("Begin sharpen processing...");
-    image_info_t newimage = sharpen_image_data(*current_image);
-    // If the previous image isn't the original image. 
-    if(previous_image->image_dat.image_arr != original_image->image_dat.image_arr){
-        // If it isn't the original  image or the current image, we free up the heap memory block. 
-        if(previous_image->image_dat.image_arr != current_image->image_dat.image_arr)
-            free_image_data_mem(previous_image); 
-        
-        // After we have free the previous image data's block, save the previous 
-        // "current" image. 
-        previous_image = current_image; 
-    }
-    // The current image is then set to the new image. 
-    *current_image = newimage; 
-    printf("Sharpening has been processed\n");
-}
-
-void process_hue(image_info_t *previous_image, image_info_t *current_image, image_info_t *original_image){
-    printf("Begining hue processing...");
-    uint16_t hue = prompt_hue(); 
-    image_info_t newimage = change_hue(*current_image, (double)hue);
-    // If the previous image isn't the original image. 
-    if(previous_image->image_dat.image_arr != original_image->image_dat.image_arr){
-        // If it isn't the original  image or the current image, we free up the heap memory block. 
-        if(previous_image->image_dat.image_arr != current_image->image_dat.image_arr)
-            free_image_data_mem(previous_image); 
-        
-        // After we have free the previous image data's block, save the previous 
-        // "current" image. 
-        previous_image = current_image; 
-    }
-    // The current image is then set to the new image. 
-    *current_image = newimage;
-
-    printf("Hue has been processed\n");
+    // Satisfy compiler warnings 
+    return 0; 
 }
 
 /*
 *   @brief Runs the main program, that prompts the user on what to do 
 *   @return bool of whether or not to end the progrma 
 */
-bool run(image_info_t *previous_image, image_info_t *current_image, image_info_t *original_image){
+uint8_t run(image_info_t *previous_image, image_info_t *current_image, image_info_t *original_image){
     print_menu(); 
 
     // Get the choice from the user. 
@@ -183,24 +115,112 @@ bool run(image_info_t *previous_image, image_info_t *current_image, image_info_t
     return 1; 
 }   
 
-int main(void){
-    
-    // Since we need to have a file to unpack in the first place: 
-    image_info_t original_image = prompt_image_filepath();
-    // Our current image. 
-    image_info_t current_image = original_image; 
-    
-    // previous image is set to current image so that can't revert to NULL. 
-    // Limitation of program is that we can only go back one change, perhaps we can make an array 
-    // Linked list, but I don't think that is necesary. 
-    image_info_t previous_image = current_image; 
+/*
+*   @brief attemps to get the file path for an image, and sends the struct over to parent function
+*   @returns image_info_t struct containing all the new image information
+*/
+image_info_t prompt_image_filepath(void){
+    image_info_t image_unpack; 
+    // Clear our the struct. 
+    memset(&image_unpack, 0, sizeof(image_unpack));
+    while(image_unpack.unpack_status != IMAGE_UNPACK_SUCCESS){
+        // Prompt our user for our file information
+        file_information_t file_info = prompt_file_information();
+        image_unpack = unpack_image(file_info.str_ptr, file_info.str_size); 
+        if(image_unpack.unpack_status != IMAGE_UNPACK_SUCCESS){
+            printf("Message couldn't be found, please try again. \n");    
+        }
+        free_file_information(&file_info); 
+    }
+    return image_unpack; 
+}
 
-    while(1){
-        // If run is returned as 0, then we know to exit the program. 
-        if(!run(&previous_image, &current_image, &original_image))
-            return 0;  
+/*
+*   @brief Prompts the user for the file name that we want to save our new file in, and then writes the file
+*   @params image_info_t image_unpack The image that we want to save as a file.  
+*   @notes there is some checking
+*/
+void prompt_file_write(image_info_t image_unpack){
+    
+    // Checking a couple red flags as to whether or not we are getting a value image unpacking tool 
+    if(image_unpack.unpack_status != IMAGE_UNPACK_SUCCESS || image_unpack.image_dat.image_arr == NULL || image_unpack.file_size == 0){
+        printf("File cannot be saved since image is unvalid\n");
     }
 
-    // Satisfy compiler warnings 
-    return 0; 
+    // Keeps on prompting until we provide a valid file name 
+    while(1){
+        file_information_t file_info = prompt_file_information(); 
+        if(file_info.str_size > 0){
+            write_file(file_info.str_ptr, file_info.str_size, image_unpack); 
+            return; 
+        }
+    }
+}
+
+/*
+*   @brief Subfunction that takes all the save image information 
+*   @notes Changes current image to reflect the "greyscale" filter
+*   @notes changes the previous image to the current image. 
+*   @params image_info_t *previous_image( the previous image that allows us to save prior work as a buffer for messups)
+*   @params image_info_t *current_image (the current image that will get changed to relfect the changes that we are asking)
+*   @params image_info_t *original_image(original source file image). This is mostly here so we can ensure that the image is processed properly. 
+*/
+void process_greyscale(image_info_t *previous_image, image_info_t *current_image, image_info_t *original_image){
+    printf("Begin greyscale processing...");
+    image_info_t newimage = greyscale_image_data(*current_image);
+    revert_checking(previous_image, current_image, original_image);
+    // The current image is then set to the new image. 
+    *current_image = newimage; 
+    printf("greyscale processing done\n"); 
+}
+
+/*
+*   @brief Subfunction that takes all the save image information. 
+*   @notes Changes current image to reflect the "sharpen" filter
+*   @notes changes the previous image to the current image. 
+*   @params image_info_t *previous_image( the previous image that allows us to save prior work as a buffer for messups)
+*   @params image_info_t *current_image (the current image that will get changed to relfect the changes that we are asking)
+*   @params image_info_t *original_image(original source file image). This is mostly here so we can ensure that the image is processed properly. 
+*/
+void process_sharpen(image_info_t *previous_image, image_info_t *current_image, image_info_t *original_image){
+    printf("Begin sharpen processing...");
+    image_info_t newimage = sharpen_image_data(*current_image);
+    revert_checking(previous_image, current_image, original_image);
+    *current_image = newimage; 
+    printf("Sharpening has been processed\n");
+}
+
+/*
+*   @brief Subfunction that takes all the save image information. 
+*   @notes Changes current image to reflect the "hue change" filter
+*   @notes changes the previous image to the current image. 
+*   @params image_info_t *previous_image( the previous image that allows us to save prior work as a buffer for messups)
+*   @params image_info_t *current_image (the current image that will get changed to relfect the changes that we are asking)
+*   @params image_info_t *original_image(original source file image). This is mostly here so we can ensure that the image is processed properly. 
+*/
+void process_hue(image_info_t *previous_image, image_info_t *current_image, image_info_t *original_image){
+    printf("Begining hue processing...");
+    uint16_t hue = prompt_hue(); 
+    image_info_t newimage = change_hue(*current_image, (double)hue);
+    revert_checking(previous_image, current_image, original_image); 
+    *current_image = newimage;
+
+    printf("Hue has been processed\n");
+}
+
+/*
+*   @brief Ensures that when we save update our previous image to the current image's results, everything is 
+*   done properly so that we are freeing up memory properly and such. 
+*/
+void revert_checking(image_info_t *previous_image, image_info_t *current_image, image_info_t *original_image){
+    // If the previous image isn't the original image. 
+    if(previous_image->image_dat.image_arr != original_image->image_dat.image_arr){
+        // If it isn't the original  image or the current image, we free up the heap memory block. 
+        if(previous_image->image_dat.image_arr != current_image->image_dat.image_arr)
+            free_image_data_mem(previous_image); 
+        
+        // After we have free the previous image data's block, save the previous 
+        // "current" image. 
+        previous_image = current_image; 
+    }
 }
